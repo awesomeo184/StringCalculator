fb5a2a0f4e1078d3bd4871e201bc6402641b39b1
awesomeo184 <awesomeo184@gmail.com>
2020-10-24 01:19:15 +0900

    메소드 분리

--- src/main/java/StringCalculatorMain.java
+++ src/main/java/StringCalculatorMain.java
@@ -1,43 +1,62 @@
 import java.util.Scanner;
 
 public class StringCalculatorMain {
+    static int getFistValue(Scanner sc) {
+        System.out.println("first operand: ");
+        return sc.nextInt();
+    }
+
+    static int getSecondVal(Scanner sc) {
+        System.out.println("second operand: ");
+        return sc.nextInt();
+    }
+
+    static String getSymbol(Scanner sc) {
+        System.out.println("operator: ");
+        return sc.next();
+    }
+
+    static int calculate(String symbol, int first, int second) {
+        int result = 0;
+        switch (symbol) {
+            case "+":
+                result = first + second;
+                System.out.println("add: " + result);
+                break;
+            case "-":
+                result = first - second;
+                System.out.println("sub: " + result);
+                break;
+            case "*":
+                result = first * second;
+                System.out.println("mul: " + result);
+                break;
+            case "/":
+                result = first / second;
+                System.out.println("div: " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operation");
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Keep calculate");
 
-        System.out.println("first operand: ");
-        int first = sc.nextInt();
+        int result = getFistValue(sc);
+
         while (true) {
-            System.out.println("operator: ");
-            String symbol = sc.next();
+            String symbol = getSymbol(sc);
             if (symbol.equals("quit")) {
                 break;
             }
+            int second = getSecondVal(sc);
+            result = calculate(symbol, result, second);
 
-            System.out.println("second operand: ");
-            int second = sc.nextInt();
-            int result = 0;
-            switch (symbol) {
-                case "+":
-                    result = first + second;
-                    System.out.println("add: " + result);
-                    break;
-                case "-":
-                    result = first - second;
-                    System.out.println("sub: " + result);
-                    break;
-                case "*":
-                    result = first * second;
-                    System.out.println("mul: " + result);
-                    break;
-                case "/":
-                    result = first / second;
-                    System.out.println("div: " + result);
-                    break;
-
-                default:
-                    System.out.println("Invalid operation");
-            }
-            first = result;
+            System.out.println(result);
         }
         sc.close();
     }
